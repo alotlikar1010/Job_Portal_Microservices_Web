fae177e5a0c4542b83732d6c02c4f0b3c9f01121
Aniket Lotlikar <alotlikar98@gmail.com>
2026-03-19 23:21:47 +0530

    added cloudinary for file upload

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,9 +1,12 @@
-import { Request, Response } from "express"
+import axios from "axios";
 import { tryCatch } from "../utils/TryCatch.js"
 import ErrorHandler from "../utils/errorHandler.js";
 import { sql } from "../utils/db.js";
 import bcrypt from "bcryptjs";
-
+import getBuffer from "../utils/buffer.js"
+import jwt from "jsonwebtoken";
+import express from "express";
+import multer from "multer";
 
 export const registerUser = tryCatch( async(req, res , next) =>{
 
@@ -34,11 +37,34 @@ export const registerUser = tryCatch( async(req, res , next) =>{
     }
     else if(role == "jobseeker") {
 
-        
-        const [user] = await sql `INSERT INTO users (name , email , password , phone_number, role )
-        Values (${name} , ${email}, ${hashPassword}, ${phoneNumber} , ${role}) RETURINING
-        user_id , name , email , role , phone_number , role , created_at`;
+        const file = req.file as Express.Multer.File
+        if(!file){
+            throw new ErrorHandler(400, "Resume file is required")
+        }
+        const filebuffer = getBuffer(file)
+        if(!filebuffer){
+            throw new ErrorHandler(500, "Failed to generate buffer")
+        }
+    const {data} = await axios.post(`${process.env.UPLOAD_SERVICES}/api/utils/upload`,{buffer:filebuffer.content})
 
+
+
+        const [user] = await sql `INSERT INTO users (name , email , password , phone_number, role, bio, resume, resume_public_id )
+        Values (${name} , ${email}, ${hashPassword}, ${phoneNumber} , ${role} ${data.url}, ${data.public_id}) RETURINING
+        user_id, name, email, phone_number, role, bio, resume, created_at`;
+        registerUser = user;    
     }
-    res.json(email)
+     const token = jwt.sign(
+        { id: registerUser?.user_id },
+        process.env.JWT_SEC as string,
+        {
+          expiresIn: "15d",
+        }
+      );
+    
+      res.json({
+        message: "user Registered",
+        registerUser,
+        token,
+      });
 })
--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,4 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import routes from './routes.js'
+import cors from 'cors'
+import {v2 as cloudinary} from "cloudinary"
 const app = express();
-dotenv.config()
+app.use(cors());
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({limit:"50mb", extended: true}));
+
+app.use("/api/utils", routes);
+
+app.listen(process.env.PORT, () =>{
+    console.log('Utils portal working')
+})
