4915f939d416624aeddbae8affc16be3e35e3030
Aniket Lotlikar <alotlikar98@gmail.com>
2026-04-13 23:23:17 +0530

    account page

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React from 'react'
 import {  useAppData } from "@/context/AppContext";
+import Loading from '@/components/loading';
 const AccountPage = () =>{
 
     const {isAuth , setUser , loading , setIsAuth,fetchApplications} = useAppData();
 
+    if (loading) return <Loading/>;
 
     return <div> Account Page  </div>;
 }
