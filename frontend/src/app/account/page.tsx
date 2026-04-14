06c1164b10d97ec4fcf1a28435ee43fd32445477
Aniket Lotlikar <alotlikar98@gmail.com>
2026-04-14 12:01:52 +0530

    frontend changes

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,14 +1,41 @@
-"use client"
-import React from 'react'
-import {  useAppData } from "@/context/AppContext";
-import Loading from '@/components/loading';
-const AccountPage = () =>{
+"use client";
+import Loading from "@/components/loading";
+import { useAppData } from "@/context/AppContext";
+import React, { useEffect } from "react";
+import Info from "./components/info";
+import Skills from "./components/skills";
+import Company from "./components/company";
+import { useRouter } from "next/navigation";
+import AppliedJobs from "./components/appliedjobs";
 
-    const {isAuth , setUser , loading , setIsAuth,fetchApplications} = useAppData();
+const AccountPage = () => {
+  const { isAuth, user, loading, applications } = useAppData();
 
-    if (loading) return <Loading/>;
+  const router = useRouter();
 
-    return <div> Account Page  </div>;
-}
+  useEffect(() => {
+    if (!isAuth && !loading) {
+      router.push("/login");
+    }
+  }, [isAuth, router, loading]);
 
-export default AccountPage
+  if (loading) return <Loading />;
+  return (
+    <>
+         {user && (
+        <div className="w-[90%] md:w-[60%] m-auto">
+          <Info user={user} isYourAccount={true} />
+          {user.role === "jobseeker" && (
+            <Skills user={user} isYourAccount={true} />
+          )}
+          {user.role === "jobseeker" && (
+            <AppliedJobs applications={applications} />
+          )}
+          {user.role === "recruiter" && <Company />}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AccountPage;
